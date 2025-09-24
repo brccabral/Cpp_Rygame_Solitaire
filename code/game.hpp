7e2214cbcfcb1e8b5c05e55893e87c1c42cedd6a
Bruno Cabral <cabral.brc@gmail.com>
2025-09-24 13:47:52 -0700

    format

--- code/game.hpp
+++ code/game.hpp
@@ -4,12 +4,14 @@
 class Game
 {
 public:
+
     Game();
     ~Game();
 
     void run();
 
 private:
-    rg::Surface* display;
+
+    rg::Surface *display;
     rl::Color bg_color = rl::Color{59, 122, 87, 255};
 };
