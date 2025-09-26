850c196f916a5c0e2ec76f40ced929601c7eedb6
Bruno Cabral <cabral.brc@gmail.com>
2025-09-25 23:00:46 -0700

    draw mats

--- code/game.cpp
+++ code/game.cpp
@@ -1,6 +1,6 @@
 #include "game.hpp"
-
 #include "resourcemanager.hpp"
+
 
 Game::Game()
 {
@@ -24,8 +24,15 @@ void Game::run()
     while (!rg::WindowCloseOrQuit())
     {
         display.Fill(bg_color);
-        cards_group.Draw(&display);
-        screen->Blit(&display, rg::math::Vector2{}, rl::BLEND_ALPHA, 0.6f);
+
+        for (const auto &mat: pile_mat_list)
+        {
+            rg::draw::rect(&display, mat_color, mat);
+        }
+
+        // cards_group.Draw(&display);
+
+        screen->Blit(&display, rg::math::Vector2{}, rl::BLEND_ALPHA, CARD_SCALE);
         rg::display::Update();
     }
 }
@@ -44,10 +51,25 @@ void Game::LoadCards()
             cards.emplace_back(suit, value);
         }
     }
+
+    pile_mat_list.emplace_back(START_X, BOTTOM_Y, MAT_WIDTH, MAT_HEIGHT);
+    pile_mat_list.emplace_back(START_X + X_SPACING, BOTTOM_Y, MAT_WIDTH, MAT_HEIGHT);
+    for (int i = 0; i < 7; ++i)
+    {
+        pile_mat_list.emplace_back(START_X + i * X_SPACING, MIDDLE_Y, MAT_WIDTH, MAT_HEIGHT);
+    }
+
+    for (int i = 0; i < 4; ++i)
+    {
+        pile_mat_list.emplace_back(START_X + i * X_SPACING, TOP_Y, MAT_WIDTH, MAT_HEIGHT);
+    }
 }
 
 void Game::Reset()
 {
+    held_cards.empty();
+    held_cards_original_pos.clear();
+
     int n_cols = 8;
     int n_rows = 7;
 
--- code/game.hpp
+++ code/game.hpp
@@ -1,7 +1,38 @@
 #pragma once
 #include <rygame.hpp>
-
 #include "card.hpp"
+
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
+#define CARD_SCALE 0.6f
+#define CARD_WIDTH 140
+#define CARD_HEIGHT 190
+#define MAT_PERCENT_OVERSIZE 1.25
+#define MAT_HEIGHT CARD_HEIGHT * MAT_PERCENT_OVERSIZE
+#define MAT_WIDTH CARD_WIDTH * MAT_PERCENT_OVERSIZE
+#define VERTICAL_MARGIN_PERCENT 0.1f
+#define HORIZONTAL_MARGIN_PERCENT 0.1f
+#define BOTTOM_Y SCREEN_HEIGHT + CARD_HEIGHT
+#define START_X (MAT_WIDTH * HORIZONTAL_MARGIN_PERCENT)
+#define TOP_Y MAT_HEIGHT * VERTICAL_MARGIN_PERCENT
+#define MIDDLE_Y (TOP_Y + MAT_HEIGHT + MAT_HEIGHT * VERTICAL_MARGIN_PERCENT)
+#define X_SPACING (MAT_WIDTH + MAT_WIDTH * HORIZONTAL_MARGIN_PERCENT)
+#define CARD_VERTICAL_OFFSET (CARD_HEIGHT * CARD_SCALE * 0.3f)
+#define BOTTOM_FACE_DOWN_PILE 0
+#define BOTTOM_FACE_UP_PILE 1
+#define PLAY_PILE_1 2
+#define PLAY_PILE_2 3
+#define PLAY_PILE_3 4
+#define PLAY_PILE_4 5
+#define PLAY_PILE_5 6
+#define PLAY_PILE_6 7
+#define PLAY_PILE_7 8
+#define TOP_PILE_1 9
+#define TOP_PILE_2 0
+#define TOP_PILE_3 1
+#define TOP_PILE_4 2
+
 
 class Game
 {
@@ -20,7 +51,11 @@ private:
     rg::Surface *screen{};
     rg::Surface display{};
     rl::Color bg_color = rl::Color{59, 122, 87, 255};
+    rl::Color mat_color = rl::Color{85, 107, 47, 255};
 
     std::vector<Card> cards{};
     rg::sprite::Group cards_group{};
+    rg::sprite::Group held_cards{};
+    std::vector<rg::Rect> held_cards_original_pos{};
+    std::vector<rg::Rect> pile_mat_list{};
 };
